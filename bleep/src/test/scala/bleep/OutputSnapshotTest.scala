c5bb3d8ad6e9ba96276354b7a6bc034dd5fd2382
Øyvind Raddum Berg <oyvind.raddum.berg@nrk.no>
2021-11-18 23:33:35 +0100

    meh

--- bleep/src/test/scala/bleep/OutputSnapshotTest.scala
+++ bleep/src/test/scala/bleep/OutputSnapshotTest.scala
@@ -1,7 +1,7 @@
 package bleep
 
 import bloop.config.{Config, ConfigCodecs}
-import com.github.plokhotnyuk.jsoniter_scala.core.{WriterConfig, writeToString}
+import com.github.plokhotnyuk.jsoniter_scala.core.{writeToString, WriterConfig}
 import org.scalactic.TripleEqualsSupport
 import org.scalatest.Assertion
 import org.scalatest.funsuite.AnyFunSuite
