44045712cdd4e6469201f31836dd0b5e0f41d05f
Øyvind Raddum Berg <oyvind.raddum.berg@nrk.no>
2021-11-21 20:12:05 +0100

    color theme

--- bleep-core/src/main/scala/bleep/logging/Pattern.scala
+++ bleep-core/src/main/scala/bleep/logging/Pattern.scala
@@ -15,21 +15,18 @@ object Pattern {
 
   @inline def colorFor(l: LogLevel): EscapeAttr =
     (l.level: @unchecked) match {
-      case LogLevel.trace.level => Color.Reset
-      case LogLevel.debug.level => Color.Green
-      case LogLevel.info.level  => Color.Blue
-      case LogLevel.warn.level  => Color.Yellow
-      case LogLevel.error.level => Color.Red
+      // https://www.ditig.com/256-colors-cheat-sheet
+      // chosen to be somewhat subtle (though I guess that's up for discussion), and to work both on white/black background
+      case LogLevel.trace.level => Color.Full(223) /* 223 NavajoWhite1	#ffd7af	rgb(255,215,175)	hsl(30,100%,84%) */
+      case LogLevel.debug.level => Color.Full(127) /* 127 Magenta3	#af00af	rgb(175,0,175)	hsl(300,100%,34%) */
+//      case LogLevel.info.level  => Color.Full(126) /* 126 MediumVioletRed	#af0087	rgb(175,0,135)	hsl(13,100%,34%) */
+      case LogLevel.info.level  => Color.Reset
+      case LogLevel.warn.level  => Color.Full(173) /* 173 LightSalmon3	#d7875f	rgb(215,135,95)	hsl(20,60%,60%) */
+      case LogLevel.error.level => Color.Full(124) /* 124 Red3	#af0000	rgb(175,0,0)	hsl(0,100%,34%) */
     }
 
-  @inline def subtleColorFor(l: LogLevel): EscapeAttr =
-    (l.level: @unchecked) match {
-      case LogLevel.trace.level => Color.Reset
-      case LogLevel.debug.level => Color.LightGreen
-      case LogLevel.info.level  => Color.LightBlue
-      case LogLevel.warn.level  => Color.LightYellow
-      case LogLevel.error.level => Color.LightRed
-    }
+  @inline def subtleColor: EscapeAttr =
+    Color.Full(102)
 
   def formatThrowable(th: Throwable): String = {
     val sw = new StringWriter()
@@ -41,7 +38,7 @@ object Pattern {
   object default extends Pattern {
     override def apply[T: Formatter](t: => Text[T], throwable: Option[Throwable], m: Metadata, ctx: Ctx): Str = {
       val Color = colorFor(m.logLevel)
-      val Subtle = subtleColorFor(m.logLevel)
+      val Subtle = subtleColor
 
       Str.join(
         Color(prefixFor(m.logLevel)),
