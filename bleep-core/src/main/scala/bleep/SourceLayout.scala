37d1fc5a09af815c351e4fa7d31f86398f9f95ca
Øyvind Raddum Berg <elacin@gmail.com>
2022-01-07 22:19:14 +0100

    Consider `target/(re)source_managed/(main|test)`

--- bleep-core/src/main/scala/bleep/SourceLayout.scala
+++ bleep-core/src/main/scala/bleep/SourceLayout.scala
@@ -3,24 +3,13 @@ package bleep
 import io.circe.{Decoder, Encoder}
 
 sealed abstract class SourceLayout(val id: String) {
-  protected def scalaSources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath]
-  protected def scalaResources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath]
+  def sources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath]
+  def resources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath]
 
-  final def sources(maybeScalaVersion: Option[Versions.Scala], maybeScope: Option[String]): JsonSet[RelPath] = {
-    val scope = maybeScope.getOrElse("")
-    maybeScalaVersion match {
-      case Some(scalaVersion) => scalaSources(scalaVersion, scope)
-      case None               => JsonSet(RelPath.force(s"src/$scope/java"))
-    }
-  }
-
-  final def resources(maybeScalaVersion: Option[Versions.Scala], maybeScope: Option[String]): JsonSet[RelPath] = {
-    val scope = maybeScope.getOrElse("")
-    maybeScalaVersion match {
-      case Some(scalaVersion) => scalaResources(scalaVersion, scope)
-      case None               => JsonSet(RelPath.force(s"src/$scope/resources"))
-    }
-  }
+  final def sources(maybeScalaVersion: Option[Versions.Scala], scope: Option[String]): JsonSet[RelPath] =
+    sources(maybeScalaVersion, scope.getOrElse(""))
+  final def resources(maybeScalaVersion: Option[Versions.Scala], scope: Option[String]): JsonSet[RelPath] =
+    resources(maybeScalaVersion, scope.getOrElse(""))
 }
 
 object SourceLayout {
@@ -39,74 +28,111 @@ object SourceLayout {
     }
 
   case object None_ extends SourceLayout("none") {
-    override protected def scalaSources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath] = JsonSet.empty
-    override protected def scalaResources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath] = JsonSet.empty
+    override def sources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] = JsonSet.empty
+    override def resources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] = JsonSet.empty
   }
 
   case object Java extends SourceLayout("java") {
-    override protected def scalaSources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath] =
-      JsonSet(RelPath.force(s"src/$scope/java"))
-    override protected def scalaResources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath] =
-      JsonSet(RelPath.force(s"src/$scope/resources"))
+    override def sources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] =
+      JsonSet(
+        RelPath.force(s"src/$scope/java"),
+        srcManaged(scope)
+      )
+
+    override def resources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] =
+      JsonSet(
+        RelPath.force(s"src/$scope/resources"),
+        resourceManaged(scope)
+      )
   }
 
   case object Normal extends SourceLayout("normal") {
-    override protected def scalaSources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath] =
-      JsonSet(
-        RelPath.force(s"src/$scope/scala"),
-        RelPath.force(s"src/$scope/java"),
-        RelPath.force(s"src/$scope/scala-${scalaVersion.binVersion}"),
-        RelPath.force(s"src/$scope/scala-${scalaVersion.epoch}"),
-        srcManaged(scalaVersion, scope)
-      )
+    override def sources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] =
+      maybeScalaVersion match {
+        case Some(scalaVersion) =>
+          JsonSet(
+            RelPath.force(s"src/$scope/scala"),
+            RelPath.force(s"src/$scope/java"),
+            RelPath.force(s"src/$scope/scala-${scalaVersion.binVersion}"),
+            RelPath.force(s"src/$scope/scala-${scalaVersion.epoch}"),
+            srcManaged(scalaVersion, scope),
+            srcManaged(scope)
+          )
+        case None => JsonSet.empty
+      }
+    override def resources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] =
+      maybeScalaVersion match {
+        case Some(scalaVersion) =>
+          JsonSet(
+            resourceManaged(scalaVersion, scope),
+            resourceManaged(scope),
+            RelPath.force(s"src/$scope/resources")
+          )
 
-    override protected def scalaResources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath] =
-      JsonSet(
-        resourceManaged(scalaVersion, scope),
-        RelPath.force(s"src/$scope/resources")
-      )
+        case None => JsonSet.empty
+      }
   }
 
   case object CrossPure extends SourceLayout("cross-pure") {
-    override protected def scalaSources(scalaVersion: Versions.Scala, scope: String) =
-      JsonSet(
-        RelPath.force(s"src/$scope/scala"),
-        RelPath.force(s"src/$scope/scala-${scalaVersion.binVersion}"),
-        RelPath.force(s"src/$scope/scala-${scalaVersion.epoch}"),
-        RelPath.force(s"src/$scope/java"),
-        RelPath.force(s"../src/$scope/scala-${scalaVersion.binVersion}"),
-        RelPath.force(s"../src/$scope/scala-${scalaVersion.epoch}"),
-        RelPath.force(s"../src/$scope/scala"),
-        srcManaged(scalaVersion, scope)
-      )
+    override def sources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] =
+      maybeScalaVersion match {
+        case Some(scalaVersion) =>
+          JsonSet(
+            RelPath.force(s"src/$scope/scala"),
+            RelPath.force(s"src/$scope/scala-${scalaVersion.binVersion}"),
+            RelPath.force(s"src/$scope/scala-${scalaVersion.epoch}"),
+            RelPath.force(s"src/$scope/java"),
+            RelPath.force(s"../src/$scope/scala-${scalaVersion.binVersion}"),
+            RelPath.force(s"../src/$scope/scala-${scalaVersion.epoch}"),
+            RelPath.force(s"../src/$scope/scala"),
+            srcManaged(scalaVersion, scope),
+            srcManaged(scope)
+          )
+        case None => JsonSet.empty
+      }
 
-    override protected def scalaResources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath] =
-      JsonSet(
-        RelPath.force(s"src/$scope/resources"),
-        RelPath.force(s"../src/$scope/resources"),
-        resourceManaged(scalaVersion, scope)
-      )
+    override def resources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] =
+      maybeScalaVersion match {
+        case Some(scalaVersion) =>
+          JsonSet(
+            RelPath.force(s"src/$scope/resources"),
+            RelPath.force(s"../src/$scope/resources"),
+            resourceManaged(scalaVersion, scope),
+            resourceManaged(scope)
+          )
+        case None => JsonSet.empty
+      }
   }
 
   case object CrossFull extends SourceLayout("cross-full") {
-    override protected def scalaSources(scalaVersion: Versions.Scala, scope: String) =
-      JsonSet(
-        RelPath.force(s"src/$scope/scala"),
-        RelPath.force(s"src/$scope/scala-${scalaVersion.binVersion}"),
-        RelPath.force(s"src/$scope/scala-${scalaVersion.epoch}"),
-        RelPath.force(s"src/$scope/java"),
-        RelPath.force(s"../shared/src/$scope/scala-${scalaVersion.binVersion}"),
-        RelPath.force(s"../shared/src/$scope/scala-${scalaVersion.epoch}"),
-        RelPath.force(s"../shared/src/$scope/scala"),
-        srcManaged(scalaVersion, scope)
-      )
+    override def sources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] =
+      maybeScalaVersion match {
+        case Some(scalaVersion) =>
+          JsonSet(
+            RelPath.force(s"src/$scope/scala"),
+            RelPath.force(s"src/$scope/scala-${scalaVersion.binVersion}"),
+            RelPath.force(s"src/$scope/scala-${scalaVersion.epoch}"),
+            RelPath.force(s"src/$scope/java"),
+            RelPath.force(s"../shared/src/$scope/scala-${scalaVersion.binVersion}"),
+            RelPath.force(s"../shared/src/$scope/scala-${scalaVersion.epoch}"),
+            RelPath.force(s"../shared/src/$scope/scala"),
+            srcManaged(scalaVersion, scope),
+            srcManaged(scope)
+          )
+        case None => JsonSet.empty
+      }
 
-    override protected def scalaResources(scalaVersion: Versions.Scala, scope: String): JsonSet[RelPath] =
-      JsonSet(
-        RelPath.force(s"src/$scope/resources"),
-        RelPath.force(s"../shared/src/$scope/resources"),
-        resourceManaged(scalaVersion, scope)
-      )
+    override def resources(maybeScalaVersion: Option[Versions.Scala], scope: String): JsonSet[RelPath] =
+      maybeScalaVersion match {
+        case Some(scalaVersion) =>
+          JsonSet(
+            RelPath.force(s"src/$scope/resources"),
+            RelPath.force(s"../shared/src/$scope/resources"),
+            resourceManaged(scalaVersion, scope),
+            resourceManaged(scope)
+          )
+        case None => JsonSet.empty
+      }
   }
 
   private def resourceManaged(scalaVersion: Versions.Scala, scope: String): RelPath = {
@@ -115,9 +141,15 @@ object SourceLayout {
     RelPath.force(s"target/scala-$v/resource_managed/$scope")
   }
 
+  private def resourceManaged(scope: String): RelPath =
+    RelPath.force(s"target/resource_managed/$scope")
+
   private def srcManaged(scalaVersion: Versions.Scala, scope: String): RelPath = {
     // seems like a surprising default in sbt
     val v = if (scalaVersion.is3) scalaVersion.scalaVersion else scalaVersion.binVersion
     RelPath.force(s"target/scala-$v/src_managed/$scope")
   }
+
+  private def srcManaged(scope: String): RelPath =
+    RelPath.force(s"target/src_managed/$scope")
 }
