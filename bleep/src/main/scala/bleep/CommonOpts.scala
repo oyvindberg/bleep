5681d01ca41501a25a4b86384e269240a396ecf7
Øyvind Raddum Berg <elacin@gmail.com>
2022-01-16 00:00:09 +0100

    Add --no-color. needs a refactor here

--- bleep/src/main/scala/bleep/CommonOpts.scala
+++ bleep/src/main/scala/bleep/CommonOpts.scala
@@ -2,9 +2,11 @@ package bleep
 
 import com.monovore.decline.Opts
 
-case class CommonOpts()
+case class CommonOpts(noColor: Boolean)
 
 object CommonOpts {
   val opts: Opts[CommonOpts] =
-    Opts(CommonOpts())
+    Opts.flag("no-color", "enable CI-friendly output").orNone.map { maybeNoColor =>
+      CommonOpts(noColor = maybeNoColor.isDefined)
+    }
 }
