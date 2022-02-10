d2335cda25972cc4b4a3bfe4a09df7ac28f2df84
Øyvind Raddum Berg <elacin@gmail.com>
2022-02-11 00:08:36 +0100

    Faster `clean`

--- bleep/src/main/scala/bleep/commands/Clean.scala
+++ bleep/src/main/scala/bleep/commands/Clean.scala
@@ -3,15 +3,23 @@ package commands
 
 import bleep.internal.FileUtils
 
-import java.nio.file.Files
+import java.nio.file.{Files, Path, Paths}
+import scala.util.Properties
 
 case class Clean(started: Started, opts: CommonOpts, projects: Option[List[model.CrossProjectName]]) extends BleepCommand {
-  override def run(): Unit =
-    started.chosenProjects(projects).foreach { projectName =>
-      val outFolder = started.bloopFiles(projectName).forceGet.project.out
-      if (Files.exists(outFolder)) {
-        FileUtils.deleteDirectory(outFolder)
-        started.logger.info(s"Deleted $outFolder")
+  override def run(): Unit = {
+    val outDirectories: List[Path] =
+      started.chosenProjects(projects).map(projectName => started.bloopFiles(projectName).forceGet.project.out).filter(Files.exists(_))
+
+    if (Properties.isWin) {
+      outDirectories.foreach { directory =>
+        FileUtils.deleteDirectory(directory)
+        started.logger.info(s"Deleted $directory")
       }
+    } else {
+      // fast path
+      cli(s"rm -Rf ${outDirectories.map(p => s"'$p'").mkString(" ")}")(Paths.get("/tmp"))
+      outDirectories.foreach(directory => started.logger.info(s"Deleted $directory"))
     }
+  }
 }
