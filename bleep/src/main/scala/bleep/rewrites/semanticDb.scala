18d5110d0cbf3493c47227743571cb6309600ebc
Øyvind Raddum Berg <elacin@gmail.com>
2022-02-28 00:26:41 +0100

    bsp: no longer drop .semanticdb files scattered around the source directories

--- bleep/src/main/scala/bleep/rewrites/semanticDb.scala
+++ bleep/src/main/scala/bleep/rewrites/semanticDb.scala
@@ -10,18 +10,15 @@ case class semanticDb(buildPaths: BuildPaths) extends Rewrite {
   def apply(name: model.CrossProjectName, explodedProject: model.Project): model.Project =
     explodedProject.scala match {
       case Some(s @ model.Scala(Some(version), _, _, _)) =>
-        val projectPaths: ProjectPaths = buildPaths.from(name, explodedProject)
-        val newOptions: Options = Options(
-          s.options.values ++ List(compilerOption(version), targetRootOptions(version, projectPaths)) ++ sourceRootOptions(version)
-        )
-
+        val projectPaths = buildPaths.from(name, explodedProject)
+        val addedScalacOptions = List(Some(compilerOption(version)), targetRootOptions(version, projectPaths), sourceRootOptions(version)).flatten
         val compilerPlugins: JsonSet[Dep] =
           compilerPlugin(version) match {
             case Some(compilerPlugin) => JsonSet(s.compilerPlugins.values + compilerPlugin)
             case None                 => s.compilerPlugins
           }
 
-        explodedProject.copy(scala = Some(s.copy(compilerPlugins = compilerPlugins, options = newOptions)))
+        explodedProject.copy(scala = Some(s.copy(compilerPlugins = compilerPlugins, options = Options(s.options.values ++ addedScalacOptions))))
 
       case _ => explodedProject
     }
@@ -39,11 +36,15 @@ case class semanticDb(buildPaths: BuildPaths) extends Rewrite {
     }
   }
 
-  def targetRootOptions(version: Versions.Scala, projectPaths: ProjectPaths): Options.Opt =
-    if (version.is3)
-      Options.Opt.WithArgs("-semanticdb-target", List(projectPaths.classes.toString))
-    else
-      Options.Opt.Flag(s"-P:semanticdb:targetroot:${projectPaths.classes}")
+  def targetRootOptions(version: Versions.Scala, projectPaths: ProjectPaths): Option[Options.Opt] =
+    if (version.is3) {
+      // this is copied from sbt, but wrong. semanticdbTarget ends up as empty string,
+      // and the .semanticdb files ends up in the source folders.
+      // the right thing seems to happen without the flag set at all, so that's where we'll leave it.
+      // Some(Options.Opt.WithArgs("-semanticdb-target", List(projectPaths.classes.toString)))
+      None
+    } else
+      Some(Options.Opt.Flag(s"-P:semanticdb:targetroot:${projectPaths.classes}"))
 
   def sourceRootOptions(version: Versions.Scala): Option[Options.Opt] =
     if (version.is3) { None }
