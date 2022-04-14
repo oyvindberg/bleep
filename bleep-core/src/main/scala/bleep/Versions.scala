bb944446d15ca4309c982d7f19fca9182aacea3d
Scala Steward <me@scala-steward.org>
2022-04-14 02:38:37 +0200

    Update scala3-library to 3.1.2

--- bleep-core/src/main/scala/bleep/Versions.scala
+++ bleep-core/src/main/scala/bleep/Versions.scala
@@ -38,7 +38,7 @@ object Versions {
 
   val Scala212 = Scala("2.12.15")
   val Scala213 = Scala("2.13.6")
-  val Scala3 = Scala("3.1.1")
+  val Scala3 = Scala("3.1.2")
 
   case class ScalaJs(scalaJsVersion: String) {
     require(scalaJsVersion.nonEmpty)
