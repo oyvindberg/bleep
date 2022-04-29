e5c482e025206864a5aef4e1ae94e9f3d53b3e2c
Erlend Hamnaberg <erlend@hamnaberg.net>
2022-04-29 14:51:44 +0200

    No need to extract files, that happens automatically

--- .github/prepare-release.sh
+++ .github/prepare-release.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 mkdir -p work/release
-(cd artifacts && unzip linux-binary.zip)
-(cd artifacts && unzip macos-binary.zip)
-(cd artifacts && unzip windows-binary.zip)
-chmod 755 artifacts/bleep-linux
-chmod 755 artifacts/bleep-macos
+chmod 755 artifacts/linux-binary/bleep-linux
+chmod 755 artifacts/macos-binary/bleep-macos
 
-mv artifacts/bleep-linux "work/release/bleep-$VERSION-x86-64-pc-linux"
-mv artifacts/bleep-macos "work/release/bleep-$VERSION-x86-64-apple-darwin"
+mv artifacts/linux-binary/bleep-linux "work/release/bleep-$VERSION-x86-64-pc-linux"
+mv artifacts/macos-binary/bleep-macos "work/release/bleep-$VERSION-x86-64-apple-darwin"
+mv artifacts/windows-binary/bleep-windows.exe artifacts/windows-binary/bleep.exe
 
 gzip "work/release/bleep-$VERSION-x86-64-pc-linux"
 gzip "work/release/bleep-$VERSION-x86-64-apple-darwin"
 CWD=$(pwd -P)
-(cd artifacts && zip -r "$CWD/work/release/bleep-$VERSION-x86-64-pc-win32.zip" *.exe)
+(cd artifacts/windows-binary && zip -r "$CWD/work/release/bleep-$VERSION-x86-64-pc-win32.zip" *.exe)
