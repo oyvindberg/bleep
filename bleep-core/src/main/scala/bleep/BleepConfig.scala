oyvindberg/bleep

commit acdd7afc8e32dda7486649dcde7f7fd99898508d

yaml instead of json (#148)
* Add support for YAML: configuration can now also be in bleep.yaml

* fail-fast: false

* snakeyaml 1.30

* GraalVM snapshot

* debug

* quite untested, but switch to yaml only:

concerns the following files:
$PROJECT/bleep.yaml (build file)
$PROJECT/.bleep/bsp/project-selection.yaml (used to only mount some subset of projects in IDE)
$HOME/.config/bleep/config.yaml (user-level config)

* sorting, deduplication

* fmt

* fork and vendor circe-yaml, update to the new snakeyaml library called snakeyaml-engine.

* meh formatting

* dep

* put circe-yaml fork in a better package

Co-authored-by: Øyvind Raddum Berg <elacin@gmail.com>

bleep-core/src/main/scala/bleep/BleepConfig.scala
@@ -1,11 +1,10 @@
 package bleep
 
 import bleep.bsp.CompileServerMode
-import bleep.internal.{FileUtils, Lazy}
+import bleep.internal.{asYamlString, FileUtils, Lazy}
 import bleep.logging.Logger
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import io.circe.jawn.decode
-import io.circe.syntax.EncoderOps
 import io.circe.{Decoder, Encoder}
 
 import java.nio.file.Files
@@ -27,14 +26,14 @@ object BleepConfig {
   implicit val encoder: Encoder[BleepConfig] = deriveEncoder
 
   def store(logger: Logger, userPaths: UserPaths, config: BleepConfig): Unit = {
-    FileUtils.writeString(userPaths.configJson, config.asJson.spaces2)
-    logger.withContext(userPaths.configJson).debug(s"wrote")
+    FileUtils.writeString(userPaths.configYaml, asYamlString(config))
+    logger.withContext(userPaths.configYaml).debug(s"wrote")
   }
 
   def load(userPaths: UserPaths): Either[BuildException, Option[BleepConfig]] =
-    if (FileUtils.exists(userPaths.configJson)) {
-      decode[BleepConfig](Files.readString(userPaths.configJson)) match {
-        case Left(e)       => Left(new BuildException.InvalidJson(userPaths.configJson, e))
+    if (FileUtils.exists(userPaths.configYaml)) {
+      decode[BleepConfig](Files.readString(userPaths.configYaml)) match {
+        case Left(e)       => Left(new BuildException.InvalidJson(userPaths.configYaml, e))
         case Right(config) => Right(Some(config))
       }
     } else Right(None)

bleep-core/src/main/scala/bleep/bsp/ProjectSelection.scala
@@ -1,8 +1,7 @@
 package bleep.bsp
 
-import bleep.internal.FileUtils
+import bleep.internal.{asYamlString, FileUtils}
 import bleep.{BuildException, BuildPaths}
-import io.circe.syntax.EncoderOps
 
 import java.nio.file.Files
 
@@ -10,16 +9,16 @@ object ProjectSelection {
   def store(buildPaths: BuildPaths, maybeSelectedProjectGlobs: Option[List[String]]): Unit =
     maybeSelectedProjectGlobs match {
       case Some(selectedProjectGlobs) =>
-        FileUtils.writeString(buildPaths.bspProjectSelectionJsonFile, selectedProjectGlobs.asJson.spaces2)
+        FileUtils.writeString(buildPaths.bspProjectSelectionYamlFile, asYamlString(selectedProjectGlobs))
       case None =>
-        Files.deleteIfExists(buildPaths.bspProjectSelectionJsonFile)
+        Files.deleteIfExists(buildPaths.bspProjectSelectionYamlFile)
         ()
     }
 
   def load(buildPaths: BuildPaths): Either[BuildException, Option[List[String]]] =
-    if (FileUtils.exists(buildPaths.bspProjectSelectionJsonFile)) {
-      io.circe.parser.decode[List[String]](Files.readString(buildPaths.bspProjectSelectionJsonFile)) match {
-        case Left(err)       => Left(new BuildException.InvalidJson(buildPaths.bspProjectSelectionJsonFile, err))
+    if (FileUtils.exists(buildPaths.bspProjectSelectionYamlFile)) {
+      io.circe.parser.decode[List[String]](Files.readString(buildPaths.bspProjectSelectionYamlFile)) match {
+        case Left(err)       => Left(new BuildException.InvalidJson(buildPaths.bspProjectSelectionYamlFile, err))
         case Right(selected) => Right(Some(selected))
       }
     } else Right(None)

bleep-core/src/main/scala/io/circe/yaml12/parser/package.scala
@@ -1,0 +1,139 @@
+package io.circe.yaml12
+
+import cats.syntax.either._
+import io.circe._
+import java.io.{Reader, StringReader}
+import java.util.Optional
+import org.snakeyaml.engine.v2.composer.Composer
+import org.snakeyaml.engine.v2.constructor.StandardConstructor
+import org.snakeyaml.engine.v2.nodes._
+import org.snakeyaml.engine.v2.parser.ParserImpl
+import org.snakeyaml.engine.v2.scanner.StreamReader
+import scala.collection.JavaConverters._
+import org.snakeyaml.engine.v2.api.LoadSettings
+
+package object parser {
+
+  val settings: LoadSettings = LoadSettings.builder.build
+
+  /** Parse YAML from the given [[Reader]], returning either [[ParsingFailure]] or [[Json]]
+    * @param yaml
+    * @return
+    */
+  def parse(yaml: Reader): Either[ParsingFailure, Json] = for {
+    parsed <- parseSingle(yaml)
+    json <- yamlToJson(parsed)
+  } yield json
+
+  def parse(yaml: String): Either[ParsingFailure, Json] =
+    parse(new StringReader(yaml))
+
+  def parseDocuments(yaml: Reader): Stream[Either[ParsingFailure, Json]] = parseStream(yaml).map(yamlToJson)
+  def parseDocuments(yaml: String): Stream[Either[ParsingFailure, Json]] = parseDocuments(new StringReader(yaml))
+
+  private[this] def asScala[T](ot: Optional[T]): Option[T] =
+    if (ot.isPresent) Some(ot.get()) else None
+
+  private[this] def createComposer(reader: Reader) =
+    new Composer(settings, new ParserImpl(settings, new StreamReader(settings, reader)))
+
+  private[this] def parseSingle(reader: Reader): Either[ParsingFailure, Node] =
+    Either.catchNonFatal {
+      val composer = createComposer(reader)
+      asScala(composer.getSingleNode)
+    } match {
+      case Left(err)          => Left(ParsingFailure(err.getMessage, err))
+      case Right(None)        => Left(ParsingFailure("no document found", new RuntimeException("no document found")))
+      case Right(Some(value)) => Right(value)
+    }
+
+  private[this] def parseStream(reader: Reader) =
+    createComposer(reader).asScala.toStream
+
+  private[this] object CustomTag {
+    def unapply(tag: Tag): Option[String] = if (!tag.getValue.startsWith(Tag.PREFIX))
+      Some(tag.getValue)
+    else
+      None
+  }
+
+  private[this] class FlatteningConstructor(settings: LoadSettings) extends StandardConstructor(settings) {
+    def flatten(node: MappingNode): MappingNode = {
+      flattenMapping(node)
+      node
+    }
+
+    def construct(node: ScalarNode): Object =
+      super.construct(node)
+  }
+
+  private[this] def yamlToJson(node: Node): Either[ParsingFailure, Json] = {
+    // Isn't thread-safe internally, may hence not be shared
+    val flattener: FlatteningConstructor = new FlatteningConstructor(settings)
+
+    def convertScalarNode(node: ScalarNode) = Either
+      .catchNonFatal(node.getTag match {
+        case Tag.INT if node.getValue.startsWith("0x") || node.getValue.contains("_") =>
+          Json.fromJsonNumber(flattener.construct(node) match {
+            case int: Integer         => JsonLong(int.toLong)
+            case long: java.lang.Long => JsonLong(long)
+            case bigint: java.math.BigInteger =>
+              JsonDecimal(bigint.toString)
+            case other => throw new NumberFormatException(s"Unexpected number type: ${other.getClass}")
+          })
+        case Tag.INT | Tag.FLOAT =>
+          JsonNumber.fromString(node.getValue).map(Json.fromJsonNumber).getOrElse {
+            throw new NumberFormatException(s"Invalid numeric string ${node.getValue}")
+          }
+        case Tag.BOOL =>
+          Json.fromBoolean(flattener.construct(node) match {
+            case b: java.lang.Boolean => b
+            case _                    => throw new IllegalArgumentException(s"Invalid boolean string ${node.getValue}")
+          })
+        case Tag.NULL => Json.Null
+        case CustomTag(other) =>
+          Json.fromJsonObject(JsonObject.singleton(other.stripPrefix("!"), Json.fromString(node.getValue)))
+        case _ => Json.fromString(node.getValue)
+      })
+      .leftMap { err =>
+        ParsingFailure(err.getMessage, err)
+      }
+
+    def convertKeyNode(node: Node) = node match {
+      case scalar: ScalarNode => Right(scalar.getValue)
+      case _                  => Left(ParsingFailure("Only string keys can be represented in JSON", null))
+    }
+
+    if (node == null) {
+      Right(Json.False)
+    } else {
+      node match {
+        case mapping: MappingNode =>
+          flattener
+            .flatten(mapping)
+            .getValue
+            .asScala
+            .foldLeft(
+              Either.right[ParsingFailure, JsonObject](JsonObject.empty)
+            ) { (objEither, tup) =>
+              for {
+                obj <- objEither
+                key <- convertKeyNode(tup.getKeyNode)
+                value <- yamlToJson(tup.getValueNode)
+              } yield obj.add(key, value)
+            }
+            .map(Json.fromJsonObject)
+        case sequence: SequenceNode =>
+          sequence.getValue.asScala
+            .foldLeft(Either.right[ParsingFailure, List[Json]](List.empty[Json])) { (arrEither, node) =>
+              for {
+                arr <- arrEither
+                value <- yamlToJson(node)
+              } yield value :: arr
+            }
+            .map(arr => Json.fromValues(arr.reverse))
+        case scalar: ScalarNode => convertScalarNode(scalar)
+      }
+    }
+  }
+}

bleep-core/src/main/scala/bleep/internal/package.scala
@@ -1,6 +1,8 @@
 package bleep
 
 import coursier.Dependency
+import io.circe.Encoder
+import io.circe.syntax.EncoderOps
 
 package object internal {
 
@@ -39,4 +41,12 @@ package object internal {
 
   def throwableMessages(th: Throwable): List[String] =
     th.getMessage :: Option(th.getCause).toList.flatMap(throwableMessages)
+
+  private val printer = new io.circe.yaml12.Printer(
+    preserveOrder = true,
+    dropNullKeys = true
+  )
+
+  def asYamlString[T: Encoder](t: T): String =
+    printer.pretty(t.asJson.foldWith(ShortenAndSortJson))
 }

bleep-core/src/main/scala/io/circe/yaml12/printer/package.scala
@@ -1,0 +1,10 @@
+package io.circe.yaml12
+
+import io.circe.Json
+
+package object printer {
+
+  /** A default printer implementation using Snake YAML.
+    */
+  def print(tree: Json): String = Printer.spaces2.pretty(tree)
+}

bleep-core/src/main/scala/io/circe/yaml12/syntax/package.scala
@@ -1,0 +1,19 @@
+package io.circe.yaml12
+
+import io.circe.Json
+
+/** Provides helpful syntax that is not specific to the SnakeYAML implementation.
+  */
+package object syntax {
+
+  final class YamlSyntax(val tree: Json) extends AnyVal {
+    def spaces2: String = Printer.spaces2.pretty(tree)
+    def spaces4: String = Printer.spaces4.pretty(tree)
+  }
+
+  /** Call this to serialize a [[Json]] AST into a YAML string using the default options.
+    */
+  implicit class AsYaml(val tree: Json) {
+    def asYaml: YamlSyntax = new YamlSyntax(tree)
+  }
+}

.github/workflows/build.yml
@@ -57,6 +57,7 @@ jobs:
     runs-on: ${{ matrix.os }}
     timeout-minutes: 20
     strategy:
+      fail-fast: false
       matrix:
         include:
           - os: ubuntu-latest

bleep-cli/src/main/scala/bleep/CommonOpts.scala
@@ -12,7 +12,7 @@ object CommonOpts {
   val ignoreVersionInBuildFile: Opts[Boolean] = Opts
     .flag(
       "ignore-version-in-build-file",
-      "use the current bleep binary and don't launch the one specified in bleep.json"
+      "use the current bleep binary and don't launch the one specified in bleep.yaml"
     )
     .orFalse
 

commit 9e900b4ceb4919e2220d8a40837e93e517370951

fallout from #148

bleep-core/src/main/scala/bleep/BleepConfig.scala
@@ -4,7 +4,7 @@ import bleep.bsp.CompileServerMode
 import bleep.internal.{asYamlString, FileUtils, Lazy}
 import bleep.logging.Logger
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
-import io.circe.jawn.decode
+import io.circe.yaml12.parser.decode
 import io.circe.{Decoder, Encoder}
 
 import java.nio.file.Files

bleep-core/src/main/scala/bleep/bsp/ProjectSelection.scala
@@ -2,6 +2,7 @@ package bleep.bsp
 
 import bleep.internal.{asYamlString, FileUtils}
 import bleep.{BuildException, BuildPaths}
+import io.circe.yaml12.parser.decode
 
 import java.nio.file.Files
 
@@ -17,7 +18,7 @@ object ProjectSelection {
 
   def load(buildPaths: BuildPaths): Either[BuildException, Option[List[String]]] =
     if (FileUtils.exists(buildPaths.bspProjectSelectionYamlFile)) {
-      io.circe.parser.decode[List[String]](Files.readString(buildPaths.bspProjectSelectionYamlFile)) match {
+      decode[List[String]](Files.readString(buildPaths.bspProjectSelectionYamlFile)) match {
         case Left(err)       => Left(new BuildException.InvalidJson(buildPaths.bspProjectSelectionYamlFile, err))
         case Right(selected) => Right(Some(selected))
       }

bleep-core/src/main/scala/io/circe/yaml12/parser/package.scala
@@ -16,6 +16,12 @@ package object parser {
 
   val settings: LoadSettings = LoadSettings.builder.build
 
+  def decode[T: Decoder](yaml: String): Either[Error, T] =
+    for {
+      parsed <- parse(yaml)
+      decoded <- Decoder[T].decodeJson(parsed)
+    } yield decoded
+
   /** Parse YAML from the given [[Reader]], returning either [[ParsingFailure]] or [[Json]]
     * @param yaml
     * @return