db7b1246f8b37d253df5504ee81c925b0ed75bae
Øyvind Raddum Berg <elacin@gmail.com>
2022-07-01 11:33:56 +0200

    break out of rosetta on arm64, use bleeps facility for resolving graalvm jvms

--- bleep-core/src/main/scala/bleep/JvmCmd.scala
+++ bleep-core/src/main/scala/bleep/JvmCmd.scala
@@ -10,10 +10,18 @@ import scala.concurrent.{Await, ExecutionContext}
 object JvmCmd {
   def apply(logger: Logger, jvm0: Option[model.Jvm], ec: ExecutionContext): Path = {
     val jvm = jvm0.getOrElse(model.Jvm.System)
+    val architecture = JvmIndex.defaultOs() match {
+      case "darwin" =>
+        import scala.sys.process._
+        val uname = Seq("uname", "-a").!!.toLowerCase
+        if (uname.contains("arm64")) "arm64" else JvmIndex.defaultArchitecture()
+      case _ => JvmIndex.defaultArchitecture()
+    }
+
     val javaBin = Await.result(
       JavaHome()
         .withCache(
-          JvmCache().withIndex(jvm.index.getOrElse(JvmIndex.coursierIndexUrl))
+          JvmCache().withIndex(jvm.index.getOrElse(JvmIndex.coursierIndexUrl)).withArchitecture(architecture)
         )
         .javaBin(jvm.name)
         .value(ec),
