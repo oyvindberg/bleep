d0f21be6972b7eb8d136c075ffd9d425b3b86043
Øyvind Raddum Berg <elacin@gmail.com>
2022-07-01 23:16:23 +0200

    fix bleep_dev.sh (ci skip)

--- bleep_dev.sh
+++ bleep_dev.sh
@@ -3,6 +3,6 @@
 # This script can be used to run bleep from command line without building native-image
 
 # where you have checked out bleep
-bleep_git=$HOME/pr/bleep
+bleep_git=$HOME/bleep
 
-bleep -d "${bleep_git}" run bleep -- -d "$(pwd)" "$@"
+bleep -d "${bleep_git}" run bleep-cli -- --ignore-version-in-build-file -d "$(pwd)" "$@"
