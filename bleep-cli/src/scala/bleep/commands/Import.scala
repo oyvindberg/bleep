oyvindberg/bleep

commit daf180e552707ae3ddd9fa807cb3a38869a16d47

port mdoc
- rework jvm handling (don't use system jvm to start bloop, warn if no jvm is set in build)
- rework cli handling a bit

bleep-cli/src/scala/bleep/commands/Import.scala
@@ -99,8 +99,9 @@ addSbtPlugin("build.bleep" % "sbt-export-dependencies" % "0.1.0")
           "+bloopInstall",
           s"""'set ThisBuild / exportProjectsTo := baseDirectory.value / s"$importDir/sbt-export"'""",
           "+exportAllProjects"
-        ).mkString(" "),
-        logger
+        ),
+        logger,
+        "sbt"
       )
     } finally Files.delete(tempAddBloopPlugin)
   }

.gitmodules
@@ -28,3 +28,6 @@
 [submodule "snapshot-tests-in/converter"]
 	path = snapshot-tests-in/converter
 	url = git@github.com:ScalablyTyped/Converter.git
+[submodule "liberated/mdoc"]
+	path = liberated/mdoc
+	url = git@github.com:bleep-build/mdoc.git

bleep-cli/src/scala/bleep/bsp/BspImpl.scala
@@ -52,8 +52,7 @@ object BspImpl {
             Some(build.$version)
           )
         }
-        val jvmCmd = FetchJvm(pre.logger, build.jvm, ExecutionContext.fromExecutor(threads.prepareBuildExecutor))
-        SetupBloopRifle(jvmCmd, pre.userPaths, lazyResolver, bleepConfig.compileServerMode, bleepRifleLogger)
+        SetupBloopRifle(bleepConfig, pre.logger, pre.userPaths, lazyResolver, bleepRifleLogger, ExecutionContext.fromExecutor(threads.prepareBuildExecutor))
       }
 
       val workspaceDir = pre.buildPaths.bleepBloopDir.getParent

bleep-cli/src/scala/bleep/commands/CompileServerStart.scala
@@ -17,9 +17,8 @@ case class CompileServerStart(logger: Logger, userPaths: UserPaths, lazyResolver
       .rewritePersisted(logger, userPaths)(_.copy(compileServerMode = CompileServerMode.Shared))
       .flatMap { bleepConfig =>
         val threads = BloopThreads.create()
-        val jvm = FetchJvm(logger, bleepConfig.compileServerJvm, ExecutionContext.fromExecutorService(threads.jsonrpc))
         val bleepRifleLogger = new BleepRifleLogger(logger)
-        val rifleConfig = SetupBloopRifle(jvm, userPaths, lazyResolver, bleepConfig.compileServerMode, bleepRifleLogger)
+        val rifleConfig = SetupBloopRifle(bleepConfig, logger, userPaths, lazyResolver, bleepRifleLogger, ExecutionContext.fromExecutorService(threads.jsonrpc))
         if (BloopRifle.check(rifleConfig, bleepRifleLogger)) {
           Right(logger.info("Compile server is already running"))
         } else {

bleep-cli/src/scala/bleep/commands/CompileServerStop.scala
@@ -20,8 +20,7 @@ case class CompileServerStop(logger: Logger, userPaths: UserPaths, lazyResolver:
 
           case mode @ CompileServerMode.Shared =>
             val bleepRifleLogger = new BleepRifleLogger(logger)
-            val rifleConfig =
-              SetupBloopRifle(FetchJvm(logger, bleepConfig.compileServerJvm, ExecutionContext.global), userPaths, lazyResolver, mode, bleepRifleLogger)
+            val rifleConfig = SetupBloopRifle(bleepConfig, logger, userPaths, lazyResolver, bleepRifleLogger, ExecutionContext.global)
             if (BloopRifle.check(rifleConfig, bleepRifleLogger)) {
               BloopRifle.exit(rifleConfig, FileUtils.TempDir, bleepRifleLogger)
             } else

bleep-core/src/scala/bleep/BleepCommandRemote.scala
@@ -36,7 +36,7 @@ abstract class BleepCommandRemote(started: Started) extends BleepCommand {
 
     val bleepRifleLogger = new BleepRifleLogger(started.logger)
     val bloopRifleConfig: BloopRifleConfig =
-      SetupBloopRifle(started.jvmCommand, started.prebootstrapped.userPaths, started.resolver, bleepConfig.compileServerMode, bleepRifleLogger)
+      SetupBloopRifle(bleepConfig, started.logger, started.prebootstrapped.userPaths, started.resolver, bleepRifleLogger, started.executionContext)
     val buildClient: BspClientDisplayProgress =
       BspClientDisplayProgress(started.logger)
 

bleep-core/src/scala/bleep/BleepConfig.scala
@@ -13,7 +13,18 @@ case class BleepConfig(
     compileServerMode: CompileServerMode,
     compileServerJvm: Option[model.Jvm],
     authentications: Option[CoursierResolver.Authentications]
-)
+) {
+  def jvmOrSetDefault(logger: Logger, userPaths: UserPaths): model.Jvm =
+    compileServerJvm match {
+      case Some(jvm) => jvm
+      case None =>
+        val defaultJvm = model.Jvm.graalvm
+        val newConfig = copy(compileServerJvm = Some(defaultJvm))
+        logger.warn(s"Defaulting bleep config to use $defaultJvm for compile server")
+        BleepConfig.store(logger, userPaths, newConfig)
+        defaultJvm
+    }
+}
 
 object BleepConfig {
   val default = BleepConfig(

bleep-core/src/scala/bleep/FetchJvm.scala
@@ -11,8 +11,7 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
 
 object FetchJvm {
-  def apply(logger: Logger, jvm0: Option[model.Jvm], ec: ExecutionContext): Path = {
-    val jvm = jvm0.getOrElse(model.Jvm.System)
+  def apply(logger: Logger, jvm: model.Jvm, ec: ExecutionContext): Path = {
     val architecture = JvmIndex.defaultOs() match {
       case "darwin" =>
         import scala.sys.process._
@@ -27,7 +26,7 @@ object FetchJvm {
       .withIndex(jvm.index.getOrElse(JvmIndex.coursierIndexUrl))
       .withArchitecture(architecture)
     val javaBin = Await.result(JavaHome().withCache(jvmCache).javaBin(jvm.name).value(ec), Duration.Inf)
-    logger.withContext(javaBin).debug(s"Resolved JVM ${jvm0.map(_.name).getOrElse("")}")
+    logger.withContext(javaBin).debug(s"Resolved JVM ${jvm.name}")
     javaBin
   }
 }

commit 785f939843eea5a5539cb83e2ed494a5d1b17baa

import: fix doubly escaped sbt command (fallout from daf180e5)

bleep-cli/src/scala/bleep/commands/Import.scala
@@ -95,9 +95,9 @@ addSbtPlugin("build.bleep" % "sbt-export-dependencies" % "0.1.0")
       cli(
         List(
           "sbt",
-          s"""'set Global / bloopConfigDir := baseDirectory.value / s"$importDir/bloop/$${scalaBinaryVersion.value}"'""",
+          s"""set Global / bloopConfigDir := baseDirectory.value / s"$importDir/bloop/$${scalaBinaryVersion.value}"""",
           "+bloopInstall",
-          s"""'set ThisBuild / exportProjectsTo := baseDirectory.value / s"$importDir/sbt-export"'""",
+          s"""set ThisBuild / exportProjectsTo := baseDirectory.value / s"$importDir/sbt-export"""",
           "+exportAllProjects"
         ),
         logger,