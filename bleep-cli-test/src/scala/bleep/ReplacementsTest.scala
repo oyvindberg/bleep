9cb9fbf6595e497ffa7aa4fbd5e9dae49b6a906b
Øyvind Raddum Berg <elacin@gmail.com>
2022-07-19 11:34:18 +0200

    replacements: only infer template strings on symbol and word boundaries

---
+++ bleep-cli-test/src/scala/bleep/ReplacementsTest.scala
@@ -0,0 +1,33 @@
+package bleep
+
+import bleep.internal.Replacements
+import org.scalactic.TripleEqualsSupport
+import org.scalatest.funsuite.AnyFunSuite
+
+class ReplacementsTest extends AnyFunSuite with TripleEqualsSupport {
+  val mainScope = Replacements.scope("main")
+
+  test("infers templates on whole path segment") {
+    val pre = RelPath.force("src/main/scala")
+    val post = mainScope.templatize.relPath(pre)
+    assert(post === RelPath.force("src/${SCOPE}/scala"))
+  }
+
+  test("does not infer template for partial matches in paths") {
+    val pre = RelPath.force("src/amain/scala")
+    val post = mainScope.templatize.relPath(pre)
+    assert(post === pre)
+  }
+
+  test("infer template after between special symbols") {
+    val pre = RelPath.force("src/a-main_b/scala")
+    val post = mainScope.templatize.relPath(pre)
+    assert(post === RelPath.force("src/a-${SCOPE}_b/scala"))
+  }
+
+  test("unconditionally fills values") {
+    val pre = RelPath.force(s"src/a$${SCOPE}b/scala")
+    val post = mainScope.fill.relPath(pre)
+    assert(post === RelPath.force("src/amainb/scala"))
+  }
+}
