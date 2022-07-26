oyvindberg/bleep

commit bb2267771c11ea27ac176eaaa6dbc60a15969b65

setup-ide: look into .bsp folder before moving it out of the way

bleep-cli/src/scala/bleep/commands/SetupIde.scala
@@ -37,13 +37,20 @@ case class SetupIde(buildPaths: BuildPaths, logger: Logger, maybeSelectedProject
 
     List(
       // remove other configured BSP tools
-      buildPaths.buildDir / ".bsp",
+      Option(buildPaths.buildDir / ".bsp").filter { p =>
+        if (Files.isDirectory(p)) {
+          Files.list(p).toList.asScala.toList match {
+            case one :: Nil if one.getFileName.toString == "bleep.json" => false
+            case _                                                      => true
+          }
+        } else true
+      },
       // causes intellij to always pick sbt BSP import
-      buildPaths.buildDir / ".bloop",
+      Some(buildPaths.buildDir / ".bloop").filter(FileUtils.exists),
       // cause metals to always pick sbt BSP import
-      buildPaths.buildDir / "build.sbt",
-      buildPaths.buildDir / "project"
-    ).filter(FileUtils.exists) match {
+      Some(buildPaths.buildDir / "build.sbt").filter(FileUtils.exists),
+      Some(buildPaths.buildDir / "project").filter(FileUtils.exists)
+    ).flatten match {
       case Nil => ()
       case conflicts =>
         LazyList

commit 38c8e90ab47bced4bd7d4644aa94d40718c58487

setup-ide: check if .bsp directory exists before looking inside. fallout from bb2267771c11ea27ac176eaaa6dbc60a15969b65

bleep-cli/src/scala/bleep/commands/SetupIde.scala
@@ -37,7 +37,7 @@ case class SetupIde(buildPaths: BuildPaths, logger: Logger, maybeSelectedProject
 
     List(
       // remove other configured BSP tools
-      Option(buildPaths.buildDir / ".bsp").filter { p =>
+      Option(buildPaths.buildDir / ".bsp").filter(FileUtils.exists).filter { p =>
         if (Files.isDirectory(p)) {
           Files.list(p).toList.asScala.toList match {
             case one :: Nil if one.getFileName.toString == "bleep.json" => false