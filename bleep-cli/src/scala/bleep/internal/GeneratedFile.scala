46c8c01451f5e254938cf89364f879059b32d10f
Øyvind Raddum Berg <elacin@gmail.com>
2022-08-01 23:05:05 +0200

    snapshot tests: properly handle generated files
    - cache all generated files from sbt build in a json file
    - generate script to recreate these files

--- bleep-cli/src/scala/bleep/internal/GeneratedFile.scala
+++ bleep-cli/src/scala/bleep/internal/GeneratedFile.scala
@@ -1,6 +1,8 @@
 package bleep.internal
 
 import bleep.RelPath
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
 
 /** This structure exists to pick up existing generated sources/resources in a build.
   *
@@ -9,3 +11,7 @@ import bleep.RelPath
   * This is meant to back the generated script which will put
   */
 case class GeneratedFile(isResource: Boolean, contents: String, toRelPath: RelPath)
+
+object GeneratedFile {
+  implicit val codec: Codec.AsObject[GeneratedFile] = deriveCodec[GeneratedFile]
+}
---
+++ snapshot-tests/doobie/scripts/src/scala/scripts/GenerateResources.scala
@@ -0,0 +1,67 @@
+
+package scripts
+
+import java.nio.file.Files
+
+object GenerateResources extends App {
+  bleep.bootstrap.forScript("GenerateResources") { (started, commands) =>
+    started.logger.error("This script is a placeholder! You'll need to replace the contents with code which actually generates the files you want")
+
+    
+    Vector(
+      bleep.model.CrossProjectName(bleep.model.ProjectName("doobie-core"), Some(bleep.model.CrossId("jvm212")))).foreach { crossName =>
+      val to = started.buildPaths.generatedSourcesDir(crossName).resolve("scala/doobie/buildinfo.scala")
+      started.logger.withContext(crossName).warn(s"Writing $to")
+      val content = s"""|package doobie
+      |
+      |/** Auto-generated build information. */
+      |object buildinfo {
+      |  /** Current version of doobie (1.0-5d0957d-SNAPSHOT). */
+      |  val version = "1.0-5d0957d-SNAPSHOT"
+      |  /** Build date (Wed Jul 27 01:43:11 CEST 2022). */
+      |  val date    = new java.util.Date(1658878991026L)
+      |}""".stripMargin
+      Files.createDirectories(to.getParent)
+      Files.writeString(to, content)
+    }
+
+
+
+    Vector(
+      bleep.model.CrossProjectName(bleep.model.ProjectName("doobie-core"), Some(bleep.model.CrossId("jvm213")))).foreach { crossName =>
+      val to = started.buildPaths.generatedSourcesDir(crossName).resolve("scala/doobie/buildinfo.scala")
+      started.logger.withContext(crossName).warn(s"Writing $to")
+      val content = s"""|package doobie
+      |
+      |/** Auto-generated build information. */
+      |object buildinfo {
+      |  /** Current version of doobie (1.0-5d0957d-SNAPSHOT). */
+      |  val version = "1.0-5d0957d-SNAPSHOT"
+      |  /** Build date (Wed Jul 27 01:42:56 CEST 2022). */
+      |  val date    = new java.util.Date(1658878976475L)
+      |}""".stripMargin
+      Files.createDirectories(to.getParent)
+      Files.writeString(to, content)
+    }
+
+
+
+    Vector(
+      bleep.model.CrossProjectName(bleep.model.ProjectName("doobie-core"), Some(bleep.model.CrossId("jvm3")))).foreach { crossName =>
+      val to = started.buildPaths.generatedSourcesDir(crossName).resolve("scala/doobie/buildinfo.scala")
+      started.logger.withContext(crossName).warn(s"Writing $to")
+      val content = s"""|package doobie
+      |
+      |/** Auto-generated build information. */
+      |object buildinfo {
+      |  /** Current version of doobie (1.0-5d0957d-SNAPSHOT). */
+      |  val version = "1.0-5d0957d-SNAPSHOT"
+      |  /** Build date (Mon Aug 01 00:08:15 CEST 2022). */
+      |  val date    = new java.util.Date(1659305295056L)
+      |}""".stripMargin
+      Files.createDirectories(to.getParent)
+      Files.writeString(to, content)
+    }
+
+  }
+}
