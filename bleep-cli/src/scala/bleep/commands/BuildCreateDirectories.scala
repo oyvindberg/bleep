312abb2667d8c58375a95434b93825f15f63e9a8
Øyvind Raddum Berg <elacin@gmail.com>
2022-08-02 21:01:17 +0200

    Add `build create-directories` command

---
+++ bleep-cli/src/scala/bleep/commands/BuildCreateDirectories.scala
@@ -0,0 +1,40 @@
+package bleep
+package commands
+
+import bleep.internal.FileUtils
+
+import java.nio.file.{Files, Path}
+import scala.collection.immutable
+
+case class BuildCreateDirectories(started: Started, projects: List[model.CrossProjectName]) extends BleepCommand {
+  override def run(): Either[BuildException, Unit] = {
+    val dirsWithProject: Map[Path, List[model.CrossProjectName]] =
+      projects
+        .flatMap { crossName =>
+          val paths = started.projectPaths(crossName)
+          val dirs = List(
+            // don't generate folders for generated sources
+            paths.sourcesDirs.fromSourceLayout,
+            paths.sourcesDirs.fromJson,
+            paths.resourcesDirs.fromSourceLayout,
+            paths.resourcesDirs.fromJson
+          ).flatten
+          dirs.map(dir => (dir, crossName))
+        }
+        .groupBy { case (dir, _) => dir }
+        .map { case (dir, tuples) => (dir, tuples.map { case (_, crossName) => crossName }) }
+
+    dirsWithProject.foreach { case (dir, crossNames) =>
+      val logger = started.logger.withContext("for projects", crossNames)
+
+      if (FileUtils.exists(dir)) {
+        logger.info(s"Already exists: $dir")
+      } else {
+        Files.createDirectories(dir)
+        logger.info(s"Created $dir")
+      }
+    }
+
+    Right(())
+  }
+}
