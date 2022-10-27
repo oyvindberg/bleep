6c14690783d51254170e5e21cbdb9f96ccec4f2f
Øyvind Raddum Berg <elacin@gmail.com>
2022-10-27 02:20:19 +0200

    add integration tests

--- scripts/src/scala/bleep/scripts/GenVideos.scala
+++ scripts/src/scala/bleep/scripts/GenVideos.scala
@@ -7,7 +7,6 @@ import bleep.tasks._
 
 import java.nio.file.attribute.PosixFilePermissions
 import java.nio.file.{Files, Path}
-import java.util
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 
@@ -15,121 +14,49 @@ object GenVideos extends BleepScript("GenVideos") {
   val RecCmd = Path.of(getClass.getResource("/asciinema-rec_script").toURI)
   val Exec = PosixFilePermissions.fromString("rwxrwxr-x")
 
-  abstract class Video(val name: String, val rows: Int = 40, val columns: Int = 100) {
-    def script: String
-  }
-
-  val videos = List(
-    new Video("run-native") {
-      override def script: String =
-        """
-        |# create build
-        |bleep build new -p native mycli
-        |
-        |# show files
-        |find . -type f
-        |
-        |# list projects
-        |bleep projects
-        |
-        |# show build file
-        |bat bleep.yaml
-        |
-        |# show generated main file
-        |bat mycli/src/scala/com/foo/App.scala
-        |
-        |# run
-        |bleep run mycli
-        |""".stripMargin
-    },
-    new Video("run-cross-native-jvm") {
-      override def script: String =
-        s"""
-        |# create build
-        |bleep build new --platform native --platform jvm --scala 2.13 --scala 3 mycli
-        |
-        |# show files
-        |find . -type f
-        |
-        |# list projects
-        |bleep projects
-        |
-        |# show build file (part one)
-        |bat --line-range :$rows bleep.yaml
-        |# show build file (part two)
-        |bat --line-range $rows: bleep.yaml
-        |
-        |# show generated main file
-        |bat mycli/shared/src/scala/com/foo/App.scala
-        |
-        |# run
-        |bleep run mycli@native213
-        |bleep run mycli@native3
-        |bleep run mycli@jvm213
-        |bleep run mycli@jvm3
-        |""".stripMargin
-    },
-    new Video("import") {
-      override def script: String =
-        s"""
-        |# git clone an existing build
-        |git clone git@github.com:scalameta/munit.git
-        |
-        |cd munit
-        |
-        |# import into bleep. note that this is a one-time, slow step
-        |bleep import
-        |
-        |# list projects
-        |bleep projects
-        |
-        |# show build file
-        |bat --line-range :$rows bleep.yaml
-        |
-        |# generate resources
-        |bleep generate-resources
-        |
-        |# run scala 3 jvm tests for all modules
-        |bleep test jvm3
-        |""".stripMargin
-    }
-  )
-
   override def run(started: Started, commands: Commands, args: List[String]): Unit = {
     val project = started.bloopProjects(model.CrossProjectName(model.ProjectName("bleep-cli"), crossId = None))
     val ni = new NativeImagePlugin(project, started.logger, nativeImageJvm = jvmOrSystem(started))
 
-    val nativeImageBleep = if (FileUtils.exists(ni.nativeImageOutput)) {
-      started.logger.info(s"Using native-image at ${ni.nativeImageOutput}")
-      Some(ni.nativeImageOutput)
-    } else None
-
-    val env = nativeImageBleep.foldLeft(sys.env.updated("BAT_PAGER", "")) { case (env, nativeImage) =>
-      // this whole exercise is really to make "bleep-cli" look like "bleep" in the videos
-      val tempDir = Files.createTempDirectory("bleep-videos")
-      Files.createSymbolicLink(tempDir / "bleep", nativeImage)
-      val newPath = sys.env.get("PATH") match {
-        case Some(existingPath) => s"$tempDir:$existingPath"
-        case None               => tempDir.toString
+    val nativeImage =
+      List(args.headOption.map(Path.of(_)), Some(ni.nativeImageOutput)).flatten.find(FileUtils.exists) match {
+        case Some(found) =>
+          started.logger.info(s"Using native-image at $found")
+          found
+        case None =>
+          sys.error(s"Expected a native-image built at ${ni.nativeImageOutput} or provided as parameter")
       }
-      env.updated("PATH", newPath)
-    }
+
+    val env = sys.env
+      .updated("BAT_PAGER", "")
+      .removed(jsonEvents.CallerProcessAcceptsJsonEvents)
+      .updated(
+        "PATH", {
+          // this whole exercise is really to make "bleep-cli" look like "bleep" in the videos
+          val tempDir = Files.createTempDirectory("bleep-videos")
+          Files.createSymbolicLink(tempDir / "bleep", nativeImage)
+          sys.env.get("PATH") match {
+            case Some(existingPath) => s"$tempDir:$existingPath"
+            case None               => tempDir.toString
+          }
+        }
+      )
 
     implicit val ec: ExecutionContext = started.executionContext
 
-    val generating: List[Future[(RelPath, Array[Byte])]] =
-      videos.map { video =>
+    val generating: List[Future[(RelPath, String)]] =
+      Demo.all.map { demo =>
         Future {
-          val relPath = RelPath.force(s"${video.name}.cast")
-          val bytes = gen(video, env.toList, started.logger.withContext(video.name))
-          (relPath, bytes)
+          val relPath = RelPath.force(s"${demo.name}.cast")
+          val content = genVideo(demo, env.toList, started.logger.withPath(demo.name))
+          (relPath, content)
         }
       }
 
-    val generated: Map[RelPath, Array[Byte]] =
+    val generated: Map[RelPath, String] =
       Await.result(Future.sequence(generating), Duration.Inf).toMap
 
-    FileSync.syncBytes(
+    FileSync.syncStrings(
       started.buildPaths.buildDir / "docs" / "videos",
       generated,
       deleteUnknowns = FileSync.DeleteUnknowns.Yes(maxDepth = None),
@@ -137,15 +64,15 @@ object GenVideos extends BleepScript("GenVideos") {
     )
   }
 
-  def gen(video: Video, env: List[(String, String)], logger: Logger): Array[Byte] = {
-    val tempDir = Files.createTempDirectory(s"bleep-videos-${video.name}")
+  def genVideo(demo: Demo, env: List[(String, String)], logger: Logger): String = {
+    val tempDir = Files.createTempDirectory(s"bleep-videos-${demo.name}")
     // nest one level to not include script and output file in file listings
     val workDir = tempDir / "work"
     Files.createDirectories(workDir)
     logger.withContext(tempDir).debug("using temporary directory")
 
     val scriptFile = tempDir / "script"
-    FileUtils.writeString(scriptFile, video.script)
+    FileUtils.writeString(scriptFile, demo.script(Path.of("bleep")))
     Files.setPosixFilePermissions(scriptFile, Exec)
 
     val outputFile = tempDir / "output"
@@ -154,40 +81,20 @@ object GenVideos extends BleepScript("GenVideos") {
       RecCmd.toString,
       scriptFile.toString,
       "--title",
-      video.name,
+      demo.name,
       "--yes",
       "--rows",
-      video.rows.toString,
+      demo.rows.toString,
       "--col",
-      video.columns.toString,
+      demo.columns.toString,
       RelPath.relativeTo(workDir, outputFile).toString // this somehow needs to be relative
     )
 
     cli("asciinema-rec_script", workDir, cmd, cliLogger = cli.CliLogger(logger), env = env)
 
-    val bytes = Files.readAllBytes(outputFile)
-    FileUtils.deleteDirectory(workDir)
+    val content = Files.readString(outputFile)
+    FileUtils.deleteDirectory(tempDir)
 
-    replaceAll(
-      bytes,
-      tempDir.toString.getBytes,
-      "/folder".getBytes
-    )
-  }
-
-  def replaceAll(bytes: Array[Byte], from: Array[Byte], to: Array[Byte]): Array[Byte] = {
-    val b = Array.newBuilder[Byte]
-    var i = 0
-    while (i < bytes.length) {
-      val endFrom = i + from.length
-      if (endFrom < bytes.length && util.Arrays.equals(bytes, i, endFrom, from, 0, from.length)) {
-        b ++= to
-        i = endFrom
-      } else {
-        b += bytes(i)
-        i += 1
-      }
-    }
-    b.result()
+    content.replace(tempDir.toString, "/folder")
   }
 }
---
+++ scripts/src/scala/bleep/scripts/It.scala
@@ -0,0 +1,70 @@
+package bleep
+package scripts
+
+import bleep.internal.{jvmOrSystem, FileUtils}
+import bleep.logging.Logger
+import bleep.tasks._
+
+import java.nio.file.{Files, Path}
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.jdk.StreamConverters.StreamHasToScala
+
+object It extends BleepScript("It") {
+  override def run(started: Started, commands: Commands, args: List[String]): Unit = {
+    val project = started.bloopProjects(model.CrossProjectName(model.ProjectName("bleep-cli"), crossId = None))
+    val ni = new NativeImagePlugin(project, started.logger, nativeImageJvm = jvmOrSystem(started))
+
+    val nativeImage = {
+      val fromArgs = args.headOption.map { path =>
+        RelPath.apply(path) match {
+          case Left(_)        => Path.of(path)
+          case Right(relPath) => started.buildPaths.cwd / relPath
+        }
+      }
+      val possible = List(fromArgs, Some(ni.nativeImageOutput)).flatten
+      possible.find(FileUtils.exists) match {
+        case Some(found) =>
+          started.logger.info(s"Using native-image at $found")
+          found
+        case None =>
+          sys.error(
+            s"Expected a native-image built at ${ni.nativeImageOutput} or provided as parameter (provided: $args. in directory: ${Files.list(started.buildPaths.cwd).toScala(List).mkString(", ")})"
+          )
+      }
+    }
+
+    val env = sys.env.updated("BAT_PAGER", "")
+    implicit val ec: ExecutionContext = started.executionContext
+
+    Await.result(
+      Future.sequence(
+        Demo.all
+          .filterNot(_.name == "import") // todo: iterate more on integration test concept. sbt import hangs in CI and would be slow anyway
+          .map(demo => Future(runDemo(nativeImage, demo, env.toList, started.logger.withPath(demo.name))))
+      ),
+      Duration.Inf
+    )
+  }
+
+  def runDemo(bleep: Path, demo: Demo, env: List[(String, String)], logger: Logger): Unit = {
+    val tempDir = Files.createTempDirectory(s"bleep-it-${demo.name}")
+    var workDir = tempDir
+
+    var action = Option.empty[String]
+
+    demo.script(bleep).lines().map(_.trim).forEach {
+      case "" => ()
+      case comment if comment.startsWith("#") =>
+        action = Some(comment.trim.dropWhile(c => c == '#' || c == ' ').takeWhile(c => c.isUnicodeIdentifierPart || c == ' '))
+      case bat if bat.startsWith("bat") => // don't need to cat files here, and bat isn't necessarily installed everywhere
+        ()
+      case cd if cd.startsWith("cd") =>
+        workDir = workDir / cd.drop("cd ".length)
+      case line =>
+        cli(action.getOrElse(line), workDir, cmd = line.split("\\s").toList, cliLogger = cli.CliLogger(logger), env = env)
+    }
+
+    FileUtils.deleteDirectory(tempDir)
+  }
+}
