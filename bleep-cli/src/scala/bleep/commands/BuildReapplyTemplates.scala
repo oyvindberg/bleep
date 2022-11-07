4aecbd156f101b6de83dc9bf51df8ab3f0bfe7e1
Øyvind Raddum Berg <elacin@gmail.com>
2022-11-08 00:30:04 +0100

    bugfix for `bleep build templates-reapply`

--- bleep-cli/src/scala/bleep/commands/BuildReapplyTemplates.scala
+++ bleep-cli/src/scala/bleep/commands/BuildReapplyTemplates.scala
@@ -9,7 +9,8 @@ case class BuildReapplyTemplates(started: Started) extends BleepCommand {
     val build = started.build.requireFileBacked(ctx = "command templates-reapply")
     val build1 = normalizeBuild(build)
     val build2 = templatesReapply(build1)
-    yaml.writeShortened(build2.file, started.buildPaths.bleepYamlFile)
+    val build3 = normalizeBuild(build2)
+    yaml.writeShortened(build3.file, started.buildPaths.bleepYamlFile)
     Right(())
   }
 }
