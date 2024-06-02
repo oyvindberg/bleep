784f3edc2d934d6a633c7417a522080487770c6a
Øyvind Raddum Berg <elacin@gmail.com>
2024-06-02 23:26:09 +0200

    sourcegen: batch calls to same script for different cross projects (#380)

--- bleep-core/src/scala/bleep/commands/SourceGen.scala
+++ bleep-core/src/scala/bleep/commands/SourceGen.scala
@@ -2,13 +2,13 @@ package bleep
 package commands
 
 import bleep.internal.{traverseish, TransitiveProjects}
-
+import bleep.model.{CrossProjectName, ScriptDef}
 import bloop.rifle.BuildServer
 
-case class SourceGen(watch: Boolean, projects: Array[model.CrossProjectName]) extends BleepCommandRemote(watch) {
+case class SourceGen(watch: Boolean, projectNames: Array[model.CrossProjectName]) extends BleepCommandRemote(watch) {
   override def watchableProjects(started: Started): TransitiveProjects = {
     val scriptProjects = for {
-      projectName <- projects
+      projectName <- projectNames
       p = started.build.explodedProjects(projectName)
       sourceGen <- p.sourcegen.values.iterator
       scriptProject = sourceGen match {
@@ -18,9 +18,15 @@ case class SourceGen(watch: Boolean, projects: Array[model.CrossProjectName]) ex
     TransitiveProjects(started.build, scriptProjects)
   }
 
-  override def runWithServer(started: Started, bloop: BuildServer): Either[BleepException, Unit] =
-    traverseish.runAll(projects) { projectName =>
-      val scripts = started.build.explodedProjects(projectName).sourcegen.values.toList
-      Script.run(started, bloop, scripts, args = List("--project", projectName.value), watch = false)
+  override def runWithServer(started: Started, bloop: BuildServer): Either[BleepException, Unit] = {
+    val byScript: Map[ScriptDef, Array[CrossProjectName]] =
+      projectNames
+        .flatMap(projectName => started.build.explodedProjects(projectName).sourcegen.values.map(script => (script, projectName)))
+        .groupMap { case (s, _) => s } { case (_, pn) => pn }
+
+    traverseish.runAll(byScript) { case (script, projectNames) =>
+      val args = projectNames.toList.flatMap(pn => List("--project", pn.value))
+      Script.run(started, bloop, List(script), args = args, watch = false)
     }
+  }
 }
