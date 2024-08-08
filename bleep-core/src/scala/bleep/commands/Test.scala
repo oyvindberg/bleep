488ae1f90e804eb582510d60a6f5eb9581bf6069
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-08-08 22:18:16 +0200

    Removed unnecessary calls to bloop
    We only query bloop for the test suites if the -o or -x flags have been
    passed to the CLI

--- bleep-core/src/scala/bleep/commands/Test.scala
+++ bleep-core/src/scala/bleep/commands/Test.scala
@@ -31,6 +31,69 @@ case class Test(
       }
     }
 
+  private def runTests(started: Started, bloop: BuildServer, testParams: bsp4j.TestParams) = {
+    val result = bloop.buildTargetTest(testParams).get()
+
+    result.getStatusCode match {
+      case bsp4j.StatusCode.OK =>
+        started.logger.info("Tests succeeded")
+        Right(())
+      case errorCode =>
+        Left(new BspCommandFailed("tests", projects, BspCommandFailed.StatusCode(errorCode)))
+    }
+  }
+
+  private def runWithFilters(started: Started, bloop: BuildServer, targets: java.util.List[bsp4j.BuildTargetIdentifier]) = {
+    val tests = bloop.buildTargetScalaTestClasses(new bsp4j.ScalaTestClassesParams(targets)).get()
+    val testClassesUnfiltered: List[String] = tests.getItems().asScala.flatMap(_.getClasses.asScala).toList
+
+    val testClasses = testSuitesExclude.map { exclude =>
+      val filtered = testClassesUnfiltered
+        .filter(fromBuildCls => exclude.toList.find(fromCliCls => isSameSuite(fromBuildCls, fromCliCls)).isDefined)
+      if (filtered.nonEmpty) testClassesUnfiltered.diff(filtered) else List.empty
+    }
+
+    val testParams = new bsp4j.TestParams(targets)
+
+    val hasSuites: Boolean =
+      testClasses match {
+        case None =>
+          testSuitesOnly
+            .map { classes =>
+              val testClassesData = new bsp4j.ScalaTestSuites(
+                intersectTestSuites(testClassesUnfiltered, classes.toList)
+                  .map(cls => new bsp4j.ScalaTestSuiteSelection(cls, List.empty[String].asJava))
+                  .asJava,
+                List.empty[String].asJava,
+                List.empty[String].asJava
+              )
+              testParams.setData(testClassesData)
+              testParams.setDataKind(bsp4j.TestParamsDataKind.SCALA_TEST_SUITES_SELECTION)
+
+              !testClassesData.getSuites().isEmpty
+            }
+            .getOrElse(true)
+        case Some(value) =>
+          if (value.isEmpty) false
+          else {
+            val testClassesData = new bsp4j.ScalaTestSuites(
+              value.map(suite => new bsp4j.ScalaTestSuiteSelection(suite, List.empty[String].asJava)).asJava,
+              List.empty[String].asJava,
+              List.empty[String].asJava
+            )
+            testParams.setData(testClassesData)
+            testParams.setDataKind(bsp4j.TestParamsDataKind.SCALA_TEST_SUITES_SELECTION)
+            true
+          }
+      }
+
+    if (hasSuites) {
+      runTests(started, bloop, testParams)
+    } else {
+      Left(new TestSuitesNotFound(testSuitesOnly.map(_.toList).getOrElse(List.empty[String])))
+    }
+  }
+
   override def watchableProjects(started: Started): TransitiveProjects =
     TransitiveProjects(started.build, projects)
 
@@ -40,62 +103,11 @@ case class Test(
   override def runWithServer(started: Started, bloop: BuildServer): Either[BleepException, Unit] =
     DoSourceGen(started, bloop, watchableProjects(started)).flatMap { _ =>
       val targets = BleepCommandRemote.buildTargets(started.buildPaths, projects)
-      val tests = bloop.buildTargetScalaTestClasses(new bsp4j.ScalaTestClassesParams(targets)).get()
-      val testClassesUnfiltered: List[String] = tests.getItems().asScala.flatMap(_.getClasses.asScala).toList
-
-      val testClasses = testSuitesExclude.map { exclude =>
-        val filtered = testClassesUnfiltered
-          .filter(fromBuildCls => exclude.toList.find(fromCliCls => isSameSuite(fromBuildCls, fromCliCls)).isDefined)
-        if (filtered.nonEmpty) testClassesUnfiltered.diff(filtered) else List.empty
-      }
-
-      val testParams = new bsp4j.TestParams(targets)
-
-      val hasSuites: Boolean =
-        testClasses match {
-          case None =>
-            testSuitesOnly
-              .map { classes =>
-                val testClassesData = new bsp4j.ScalaTestSuites(
-                  intersectTestSuites(testClassesUnfiltered, classes.toList)
-                    .map(cls => new bsp4j.ScalaTestSuiteSelection(cls, List.empty[String].asJava))
-                    .asJava,
-                  List.empty[String].asJava,
-                  List.empty[String].asJava
-                )
-                testParams.setData(testClassesData)
-                testParams.setDataKind(bsp4j.TestParamsDataKind.SCALA_TEST_SUITES_SELECTION)
-
-                !testClassesData.getSuites().isEmpty
-              }
-              .getOrElse(true)
-          case Some(value) =>
-            if (value.isEmpty) false
-            else {
-              val testClassesData = new bsp4j.ScalaTestSuites(
-                value.map(suite => new bsp4j.ScalaTestSuiteSelection(suite, List.empty[String].asJava)).asJava,
-                List.empty[String].asJava,
-                List.empty[String].asJava
-              )
-              testParams.setData(testClassesData)
-              testParams.setDataKind(bsp4j.TestParamsDataKind.SCALA_TEST_SUITES_SELECTION)
-              true
-            }
-        }
-
-      if (hasSuites) {
-        val result = bloop.buildTargetTest(testParams).get()
-
-        result.getStatusCode match {
-          case bsp4j.StatusCode.OK =>
-            started.logger.info("Tests succeeded")
-            Right(())
-          case errorCode =>
-            println(errorCode)
-            Left(new BspCommandFailed("tests", projects, BspCommandFailed.StatusCode(errorCode)))
-        }
-      } else {
-        Left(new TestSuitesNotFound(testSuitesOnly.map(_.toList).getOrElse(List.empty[String])))
+      (testSuitesExclude, testSuitesOnly) match {
+        case (None, None) =>
+          runTests(started, bloop, new bsp4j.TestParams(targets))
+        case _ =>
+          runWithFilters(started, bloop, targets)
       }
     }
 }
